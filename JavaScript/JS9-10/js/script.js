9ac9c629ea328942bb38e5c64ae97f80cd0ce105
Nataliya Zakharenko <jobinet15@gmail.com>
2016-06-02 12:50:07 +0300

    added js

---
+++ JavaScript/JS9-10/js/script.js
@@ -0,0 +1,57 @@
+// МЕНЮ
+
+$(function() {
+  if ($.browser.msie && $.browser.version.substr(0,1)<7)
+  {
+$('li').has('ul').mouseover(function(){
+$(this).children('ul').show();
+}).mouseout(function(){
+$(this).children('ul').hide();
+})
+  }
+});
+
+// КАРУСЕЛЬ
+
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})
+
+//Чекбокс
+
+$(document).ready(function(){
+$(‘input, textarea, select, button’).uniform();
+});
